094c0517d4e2dea4fc4a8c671a52ae39c529755c
Oliver Greenaway <oliver.greenaway@outlyer.com>
2017-08-25 12:25:49 +0100

    remove a load of other stuff from metadata

--- metadata.rb
+++ metadata.rb
@@ -9,12 +9,7 @@ version '3.0.0'
 recipe 'consul::default', 'Installs and configures the Consul service.'
 recipe 'consul::client_gem', 'Installs the Consul Ruby client as a gem.'
 
-supports 'centos', '>= 6.4'
-supports 'redhat', '>= 6.4'
 supports 'ubuntu', '>= 12.04'
-supports 'solaris2'
-supports 'arch'
-supports 'windows'
 
 depends 'build-essential'
 depends 'golang'
@@ -22,7 +17,3 @@ depends 'poise', '~> 2.2'
 depends 'poise-archive', '~> 1.3'
 depends 'poise-service', '~> 1.4'
 
-source_url 'https://github.com/johnbellone/consul-cookbook' if respond_to?(:source_url)
-issues_url 'https://github.com/johnbellone/consul-cookbook/issues' if respond_to?(:issues_url)
-
-chef_version '>= 12.1' if respond_to?(:chef_version)
